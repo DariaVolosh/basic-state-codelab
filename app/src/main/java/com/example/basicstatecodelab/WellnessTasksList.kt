85dd246b8fbc9a6d1d9d1d522e36a8afae4eaebc
Dasha999a <darivolosh@gmail.com>
2023-12-01 21:43:16 +0400

    First commit

---
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
@@ -0,0 +1,25 @@
+package com.example.basicstatecodelab
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit
+) {
+    LazyColumn(modifier = modifier) {
+        items(list, key = {task -> task.id}) {task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                onClose = {onCloseTask(task)},
+                checked = task.checked,
+                onCheckedChange = {checked -> onCheckedTask(task, checked)}
+            )
+        }
+    }
+}
